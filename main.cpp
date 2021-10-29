065b9e62c1fe15c0891c75a400272b7e9c2f79f1
taka <89272346+Adnanhajifarah@users.noreply.github.com>
2021-10-29 17:44:07 +0000

    nothing

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ using namespace std;
   }
   void inputc(int x){
     while(x!=1&&x!=2&&x!=3&&x!=4){
-      cout<<endl<<"invalid Input!\n read directions and enter your choice"<<endl;
+      cout<<endl<<"Invalid Input!\n read directions and enter your choice"<<endl;
       cin>>x;
 
     }
